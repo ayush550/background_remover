ed42a789efd934e790f616a49f350081735af6d1
Ayush Chauhan <85547418+ayush550@users.noreply.github.com>
2025-07-30 13:05:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,16 +11,20 @@ def index():
 
 @app.route('/remove_bg', methods=['POST'])
 def remove_background():
+    print("Received request to remove background")
     file = request.files['image']
+    userfilename = 'default'
+    if file.filename != '':
+        userfilename = file.filename.split('.')[0]  # Get the filename without extension
+        print(f"Processing file: {userfilename}")
     input_bytes = file.read()
     output_bytes = remove(input_bytes)
-    
     output_img = Image.open(io.BytesIO(output_bytes))
     img_io = io.BytesIO()
     output_img.save(img_io, 'PNG')
     img_io.seek(0)
 
-    return send_file(img_io, mimetype='image/png', as_attachment=True, download_name='no-bg.png')
+    return send_file(img_io, mimetype='image/png', as_attachment=True, download_name=f"{userfilename}.png")
 
 if __name__ == '__main__':
     app.run(debug=True)
